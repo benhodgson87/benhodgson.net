72a21faeecd8a1d88e4006e8b045db1a75f947e3
Ben Hodgson <github@benhodgson.net>
2022-04-25 23:54:40 +0100

    Fix prerendering and incorrect favicon path

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,15 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    browser: {
+      hydrate: false
+    },
+    prerender: {
+      enabled: true,
+      default: true,
+      crawl: false,
+    }
   }
 };
 
